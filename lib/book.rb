5ce4f83a920612e9297940b26d31c35c1181b9e0
sheema <sheemashamilfarook@gmail.com>
2023-02-13 15:07:26 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,15 @@
+class Book
+    attr_accessor :title, :author, :page_count, :genre
 
+    def initialize(title, author = "Agatha Christie", page_count = 272, genre = "Mystery")
+        @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
+    end
+
+    def turn_page
+        puts turn_page = "Flipping the page...wow, you read fast!"
+    end
+end
 
